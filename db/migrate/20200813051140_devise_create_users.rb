0599e3df96b1ee04d35bacb8bda0c1c8c629faf7
wataru-nakamura <wataru4711@gmail.com>
2020-08-13 23:40:36 +0900

    新規登録・ログイン機能実装

--- db/migrate/20200813051140_devise_create_users.rb
+++ db/migrate/20200813051140_devise_create_users.rb
@@ -6,13 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
-      t.string :password,           null: false
+      t.string :encrypted_password, null: false, default: ""
       t.string :family_name,        null: false
       t.string :first_name,         null: false
-      t.string :family_namekana,    null: false
+      t.string :family_name_kana,    null: false
       t.string :first_name_kana,    null: false
       t.date   :birthday,           null: false
-      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
