6657999c978b2bd4eab4002ab7bbe663cf9c0faa
wataru-nakamura <wataru4711@gmail.com>
2020-08-14 10:59:48 +0900

    テスト関係gem導入・FactoryBot記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    gimei = Gimei.name
+    family_name {gimei.last.kanji}
+    first_name {gimei.first.kanji}
+    family_name_kana {gimei.last.katakana}
+    first_name_kana {gimei.first.katakana}
+    birthday {Faker::Date.birthday(min_age: 10, max_age: 80)}
+
+  end
+end
