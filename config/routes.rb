757a8cf2296ba9b1b2ce770850489d2f51f33e11
wataru-nakamura <wataru4711@gmail.com>
2020-08-20 18:56:18 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "products#index"
-  resources :products 
-  resources :buyers, only: [:index]
+  resources :products  do
+    resources :buyers, only: [:index, :create]
+  end
 end
