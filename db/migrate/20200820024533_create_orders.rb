9eff447824561da44906a2be533d445582664a99
wataru-nakamura <wataru4711@gmail.com>
2020-08-25 10:23:06 +0900

    テーブル設計修正

--- db/migrate/20200820024533_create_orders.rb
+++ db/migrate/20200820024533_create_orders.rb
@@ -3,7 +3,6 @@ class CreateOrders < ActiveRecord::Migration[6.0]
     create_table :orders do |t|
       t.integer :user_id, foreign_key: true
       t.integer :product_id, foreign_key: true
-      t.integer :buyer_id, foreign_key: true
       t.timestamps
     end
   end
