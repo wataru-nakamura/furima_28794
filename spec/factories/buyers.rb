8a920c219e02f3dea229e744f04d4d2241c74486
wataru-nakamura <wataru4711@gmail.com>
2020-08-26 19:25:34 +0900

    テストコード記述

--- spec/factories/buyers.rb
+++ spec/factories/buyers.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :buyer do
-    
+    postal_code {'123-4567'}
+    prefectures_id {3}
+    city {'テスト市'}
+    address {'1-1'}
+    building_name {'テストビル101'}
+    phone_number {'09012345678'}
+    association :order
   end
 end
